40987492ff24276d6af4653797291f55e7e2132e
deciduously <ben@deciduously.com>
2019-12-14 09:17:36 -0500

    Refactor

--- src/bin/aoc.rs
+++ src/bin/aoc.rs
@@ -6,13 +6,13 @@ const DAYS_IMPLEMENTED: u32 = 3;
 fn main() {
     if let Some(day) = args().nth(1) {
         if let Ok(day) = day.parse::<u32>() {
-            if day <= DAYS_IMPLEMENTED {
+            if day <= DAYS_IMPLEMENTED && day > 0 {
                 println!("Day {}", day);
                 match day {
                     1 => day1::run(),
                     2 => day2::run(),
                     3 => day3::run(),
-                    _ => eprintln!("Unimplemented day")
+                    _ => unreachable!()
                 }
             } else {
                 eprintln!("Day must be between 1 and {} inclusive", DAYS_IMPLEMENTED);
